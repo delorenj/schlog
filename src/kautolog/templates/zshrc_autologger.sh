c9d450a36a41dd6bae7218dc344596de42ce54e6
Mark Sowell <mark@marksowell.com>
2025-08-16 00:44:28 -0700

    .

--- src/kautolog/templates/zshrc_autologger.sh
+++ src/kautolog/templates/zshrc_autologger.sh
@@ -4,7 +4,7 @@ if [[ -o interactive ]] && [[ -t 1 ]] && [[ -z "$UNDER_SCRIPT" ]]; then
   export UNDER_SCRIPT=1
   base="$HOME/terminal-logs/$(date +%Y)/$(date +%m)/$(date +%d)"
   mkdir -p "$base"
-  typeset -g logbase="$base/$(hostname)-$$-$(date +%H%M%S)"
+  export logbase="$base/$(hostname)-$$-$(date +%H%M%S)"
   exec /usr/bin/script -fqe -t"$logbase.timing" "$logbase.log"
 fi
 # <<< KAUTOLOG END <<<
